4b3f94598d5f7cb9e155d65c8574f5d5190d2648
Ulisses DaSilva <Ulisses.DaSilva@QUALITEST.LOCAL>
2021-12-21 17:08:08 +0000

    update cucumber sendkyes feature

---
+++ testCases/selenium/steps/TC002_clickSendKeys.py
@@ -0,0 +1,39 @@
+import time
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@given('the user navigate to the text field section of page')
+def step_impl(context):
+    context.driver = webdriver.Chrome(ChromeDriverManager().install())
+    context.driver.implicitly_wait(10)
+    time.sleep(4)
+    context.driver.get('https://letskodeit.teachable.com')
+    context.driver.maximize_window()
+
+@when('the user select a text field and perform sendkeys action')
+def step_impl(context):
+    time.sleep(1)
+    clickLogin = context.driver.find_element(By.XPATH,"//*[@id='navbar']/div/div/div/ul/li[2]/a")
+    clickLogin.click()
+
+    time.sleep(1)
+    emailField = context.driver.find_element(By.ID,"email")
+    emailField.send_keys("test")
+
+    time.sleep(1)
+    passwordField = context.driver.find_element(By.ID,"password")
+    passwordField.send_keys("test")
+
+    time.sleep(1)
+    emailField.clear()
+
+    time.sleep(1)
+    emailField.send_keys("test")
+
+
+@then('the user should be able to sendkeys action in the page.')
+def step_impl(context):
+    print('user able to send keys')
