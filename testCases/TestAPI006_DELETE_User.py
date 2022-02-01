e0463de5418ab37d57515f78f72b22d396ef8fec
Anshujit Mishra <anshujit.mishra@QUALITEST.LOCAL>
2022-02-01 18:29:41 +0000

    Delete request update

--- testCases/TestAPI006_DELETE_User.py
+++ testCases/TestAPI006_DELETE_User.py
@@ -1,11 +1,5 @@
 import requests
 
-payload = {
-    "name": "morpheus_Automation",
-    "job": "leader_Automation"
-}
-
-resp = requests.delete("https://reqres.in/api/users/2", data=payload)
-print(resp.json())
+resp = requests.delete("https://reqres.in/api/users/2")
 print(resp.status_code)
 assert resp.status_code == 204, "Deletion Fail"
