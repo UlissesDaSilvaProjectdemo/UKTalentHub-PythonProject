f27ff879a153d7a3c937260dc71a4af4d10f2790
Ulisses DaSilva <Ulisses.DaSilva@QUALITEST.LOCAL>
2022-05-10 11:47:48 +0100

    check git files update

--- pageObjects/TC001_LoginPage.py
+++ pageObjects/TC001_LoginPage.py
@@ -9,6 +9,8 @@ class LoginPage:
     password_xpath = "//*[@id='passwd']"
     login_button_xpath = "//*[@id='SubmitLogin']"
     _login_button = "//*[@id='SubmitLogin']"
+    # _login_button = "//*[@id='SubmitLogin']"
+    #_login_button = "//*[@id='SubmitLogin']"
 
     def __init__(self, driver):
         self.driver = driver
